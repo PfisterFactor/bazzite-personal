28a5157fe160ba9662a1569583860a89aa99d763
Eric Pfister <phofidd@gaming.house>
2026-04-09 01:07:36 -0500

    feat: add realtek driver for ethernet

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,14 +10,15 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# Install Realtek r8125 2.5GbE driver (replaces slow r8169 in-kernel driver)
+dnf5 -y copr enable laurie-reeves/realtek-r8125-dkms
+dnf5 -y install dkms kernel-devel realtek-r8125-dkms
+dnf5 -y copr disable laurie-reeves/realtek-r8125-dkms
+
+# Blacklist r8169 so the r8125 driver is used for RTL8125 NICs
+echo "blacklist r8169" > /usr/lib/modprobe.d/blacklist-r8169.conf
 
 #### Example for enabling a System Unit File
 
